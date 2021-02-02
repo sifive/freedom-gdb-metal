063e0240804e3cd8c6b56886f67fb9e5e6b70d94
Kito Cheng <kito.cheng@sifive.com>
2021-02-02 18:48:44 +0800

    Auto version for dev

--- Version.mk
+++ Version.mk
@@ -2,7 +2,7 @@
 RISCV_GDB_VERSION := 10.1.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_GDB_METAL_ID := 2020.12.7
+FREEDOM_GDB_METAL_ID := dev-$(shell cd src/riscv-gdb/ && git log --pretty=format:'%h' -1)
 
 # Characteristic tags, which should be usable for matching up providers and consumers
 FREEDOM_GDB_METAL_RISCV_TAGS = rv32i rv64i m a f d c v b zfh
